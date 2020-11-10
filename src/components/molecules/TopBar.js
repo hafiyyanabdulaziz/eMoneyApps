eb1791262f0dc9dacb983dab16bea47f4f31a478
Hafiyyan Abdul Aziz <hafiyyan.123@gmail.com>
2020-11-10 16:12:48 +0700

    arrow back sudah

--- src/components/molecules/TopBar.js
+++ src/components/molecules/TopBar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import IconButton from '../atoms/IconButton'
 
 const TopBar = () => {
     return (
         <View style={styles.topbar} >
-            <Text>Halo</Text>
+            <View style={{ width: 17 }} />
+            <IconButton />
         </View>
     )
 }
@@ -14,6 +16,7 @@ export default TopBar
 const styles = StyleSheet.create({
     topbar: {
         backgroundColor: '#005690',
-        height: 56,
+        paddingVertical: 20,
+        flexDirection: 'row',
     }
 })
