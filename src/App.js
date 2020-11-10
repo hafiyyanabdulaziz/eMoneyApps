9194f981b7e41022029a136da86b42c26adfce2d
Hafiyyan Abdul Aziz <hafiyyan.123@gmail.com>
2020-11-11 06:22:30 +0700

    Transfer tinggal foto

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Login from './pages/Login'
 import Registrasi from './pages/Registrasi'
 import ScanQR from './pages/ScanQR'
 import TopUp from './pages/TopUp'
+import Transfer from './pages/Transfer'
 
 export default function App() {
   return (
-    <TopUp />
+    <Transfer />
   )
 }
--- src/pages/Transfer.js
+++ src/pages/Transfer.js
@@ -1,14 +1,38 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Button from '../components/atoms/Button'
+import Input from '../components/atoms/Input'
+import TopBar from '../components/molecules/TopBar'
 
 const Transfer = () => {
     return (
         <View>
-            <Text>Transfer</Text>
+            <TopBar title='Transfer' />
+            <View style={{ height: 214 }} />
+            <Input placeholder='Nominal Transfer' />
+            <View style={{ height: 17 }} />
+            <Input placeholder='No Handphone Penerima' />
+            <View style={{ height: 21 }} />
+            <Text style={styles.text1}>Penerima:</Text>
+            <View style={{ height: 21 }} />
+            <Text style={styles.text2}>Dendy Aditya</Text>
+            <View style={{ height: 21 }} />
+            <Button title='TRANSFER' />
         </View>
     )
 }
 
 export default Transfer
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    text1: {
+        textAlign: "center",
+        fontSize: 16,
+        fontWeight: '400',
+    },
+    text2: {
+        textAlign: "center",
+        fontSize: 16,
+        fontWeight: '300',
+    }
+})
